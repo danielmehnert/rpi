2e63cca13ddd2b84914c308daa5eb3f109a84e9f
danielmehnert <daniel.mehnert@gmail.com>
2016-12-07 11:24:23 +0100

    Improved color fading over time

--- pimodori/blnkodori.py
+++ pimodori/blnkodori.py
@@ -1,69 +1,39 @@
-import copy, random
+#!/usr/bin/env python
+
 import colorsys
 import time
-nohat = 0
+
 try:
-    #import unicornhat as uh
     import blinkt as blnkt
     blnkt.set_brightness(.1)
 except ImportError:
-    print "no Unicornhat found"
-    nohat = 1
-import time
+    print "no Blinkt! found"
 
-#if not nohat:
-#    uh.brightness(.2)
 
-def show_matrix(mat):
-    for row in mat:
-        print " ".join(str(x[0]) for x in row)
-
-def uh_show_matrix(mat, pause = .075):
-    for y in range(len(mat)):
-        for x in range(len(mat[0])):
-            uh.set_pixel(x, y, mat[x][y][0], mat[x][y][1], mat[x][y][2])
-            
-    uh.show()
-    time.sleep(pause)
-        
-
-def pomodoro(color = (255,0,0), min=25):
-    for y in range(8):
-        for x in range(8):
-            uh.set_pixel(x, y, color[0], color[1], color[2])
-    uh.show()
-    for i in range(7,-1,-1):
-        for j in range(7,-1,-1):
-            time.sleep(min*60/64)
-            uh.set_pixel(j,i, 0,0,0)
-            uh.show()
-
-def pomodoro_blnkt(color = (255,0,0), min=25):
-    blnkt.set_brightness(.1)
-    blnkt.set_all(color[0], color[1], color[2])
-    blnkt.show()
-    for p in range(8):
-        time.sleep(min*60/8)
-        blnkt.set_pixel(p, 0,0,0)
-        blnkt.set_brightness(0.1*0.9**p)
-        blnkt.show()
 
 def pomodoro_fade(color = (255,0,0), min=25):
-    blnkt.set_brightness(.1)
     blnkt.set_all(color[0], color[1], color[2])
     blnkt.show()
     for p in range(8):
-        for i in range(10):
-            time.sleep(min*60/8/10)
-            blnkt.set_pixel(p, color[0], color[1], color[2], brightness=0.1-0.01*i)
-            #blnkt.set_brightness(0.1*0.9**p)
+        for i in range(1, 256):
+            time.sleep(min*60/8/255.0)
+            r, g, b = [x-i if x > 0 else 0 for x in color]
+            blnkt.set_pixel(p, r, g, b)
             blnkt.show()
-
-
+    blnkt.set_all(color[0], color[1], color[2], brightness = .1)
+    blnkt.show()
+    for i in range(4):
+        blnkt.set_brightness(.2)
+        blnkt.show()
+        time.sleep(.1)
+        blnkt.set_brightness(.1)
+        blnkt.show()
+        time.sleep(.1)
 
 if __name__ == "__main__":
     while 1: 
-        #pomodoro_fade(min=.5)
-        #pomodoro_fade(color=(0,255,0),min=.5)
-        pomodoro_fade(min=25)
-        pomodoro_fade(color=(0,255,0), min=5)
+        #Standard Pomodoro technique schedule:
+        for i in range(4):
+            pomodoro_fade(color = (255, 0, 0), min=25)
+            pomodoro_fade(color=(0,255,0), min=5)
+        pomodoro_fade(0,0,255, min=20)
