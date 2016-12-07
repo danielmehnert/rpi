3401fa7e2bfabc6e77687c4a0b0fb721469d7433
danielmehnert <daniel.mehnert@gmail.com>
2016-12-07 12:33:57 +0100

    Improved standard schedule

--- pimodori/blnkodori.py
+++ pimodori/blnkodori.py
@@ -3,13 +3,10 @@
 import colorsys
 import time
 
-try:
-    import blinkt as blnkt
-    blnkt.set_brightness(.1)
-except ImportError:
-    print "no Blinkt! found"
 
+import blinkt as blnkt
 
+blnkt.set_brightness(.1)
 
 def pomodoro_fade(color = (255,0,0), min=25):
     blnkt.set_all(color[0], color[1], color[2])
@@ -17,11 +14,13 @@ def pomodoro_fade(color = (255,0,0), min=25):
     for p in range(8):
         for i in range(1, 256):
             time.sleep(min*60/8/255.0)
+            #slow color intensity decrease
             r, g, b = [x-i if x > 0 else 0 for x in color]
             blnkt.set_pixel(p, r, g, b)
             blnkt.show()
     blnkt.set_all(color[0], color[1], color[2], brightness = .1)
     blnkt.show()
+    #visual alert 
     for i in range(4):
         blnkt.set_brightness(.2)
         blnkt.show()
@@ -35,5 +34,6 @@ if __name__ == "__main__":
         #Standard Pomodoro technique schedule:
         for i in range(4):
             pomodoro_fade(color = (255, 0, 0), min=25)
-            pomodoro_fade(color=(0,255,0), min=5)
-        pomodoro_fade(0,0,255, min=20)
+            if i != 3: #no break before long break
+                pomodoro_fade(color=(0,255,0), min=5)
+        pomodoro_fade(color = (0,0,255), min=20)
